552a91dd536f4879e31d2f8fc20a7ee5ec71cbb4
Klooven <Klooven@users.noreply.github.com>
2018-05-19 15:45:16 +0300

    New uglify

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import eslint from 'rollup-plugin-eslint';
-import uglify from 'rollup-plugin-uglify';
+import {uglify} from 'rollup-plugin-uglify';
 
 export default {
   input: "app/js/mol.js",
