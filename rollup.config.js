0da4963f8d27f27db2752efa2a9cb251ca142b3d
Klooven <Klooven@users.noreply.github.com>
2022-12-04 15:45:28 +0200

    Bump deps

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import {nodeResolve} from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import {babel} from '@rollup/plugin-babel';
 import eslint from '@rollup/plugin-eslint';
-import {terser} from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 export default {
   input: "app/js/mol.js",
